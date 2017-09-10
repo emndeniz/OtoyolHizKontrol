c5e9599f0b592a5a363e7841e78d326c769f4599
medeniz <medeniz@netas.com.tr>
2017-09-10 16:41:01 +0300

    initial commit

---
+++ app/src/main/java/kontrol/hiz/otoyol/emndeniz/com/otoyolhizkontrol/Constants.java
@@ -0,0 +1,11 @@
+package kontrol.hiz.otoyol.emndeniz.com.otoyolhizkontrol;
+
+/**
+ * Created by emindeniz on 10/09/2017.
+ */
+
+public class Constants {
+
+    public static final int PERMISSIONS_REQUEST_LOCATION = 99;
+
+}
